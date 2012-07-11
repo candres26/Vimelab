b71d5155fc43ddfbf5a3329ddf0291e92cd09bf9
Javier Herran <ejherran.c@gamil.com>
2012-07-11 08:54:11 -0500

    TMP

--- src/Vimelab/ScontrolBundle/Controller/AsSecuController.php
+++ src/Vimelab/ScontrolBundle/Controller/AsSecuController.php
@@ -3,6 +3,7 @@
 namespace Vimelab\ScontrolBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Vimelab\ScontrolBundle\Entity\Gbcarg;
 use Vimelab\ScontrolBundle\Entity\Gbvars;
 use Symfony\Component\HttpFoundation\Response;
 use Vimelab\ScontrolBundle\Tool\Tool;
@@ -12,10 +13,70 @@ class AsSecuController extends Controller
 	public function indexAction()
 	{
 		if(Tool::isGrant($this))
-        {		
-			return $this->render('ScontrolBundle:AsSecu:index.html.twig');
+        {
+			$em = $this->getDoctrine()->getEntityManager();
+			$cargos = $em->getRepository('ScontrolBundle:Gbcarg')->findBy(array(), array('nombre' => 'ASC'));
+			
+			return $this->render('ScontrolBundle:AsSecu:index.html.twig', array('cargos' => $cargos));
 		}
 		else
 			return $this->render('ScontrolBundle::alertas.html.twig');
+	}
+	
+	public function getCargoAction()
+	{
+		if(Tool::isGrant($this))
+        {
+        	$request = $this->getRequest();
+        	if($request->isXmlHttpRequest())
+			{
+				$em = $this->getDoctrine()->getEntityManager();
+				
+				$cargos = $em->getRepository('ScontrolBundle:Gbcarg')->findBy(array(), array('nombre' => 'ASC'));
+				
+				$parr = array();
+				foreach($cargos as $caso)
+					$parr[] = $caso->getId().'=>'.$caso->getNombre();
+				
+				$parr = join('|:|', $parr);
+				$parr = $parr == '' ? '%' : $parr;
+				
+				return new Response($parr);
+			}
+			else
+				return $this->redirect($this->generateUrl('as_secu'));
+		}
+		else
+			return $this->render("ScontrolBundle::alertas.html.twig");
+	}
+	
+	public function addCargoAction()
+	{
+		if(Tool::isGrant($this))
+        {
+        	$request = $this->getRequest();
+        	if($request->isXmlHttpRequest())
+			{
+				$em = $this->getDoctrine()->getEntityManager();
+				
+				try
+				{
+					$entity = new Gbcarg();
+					$entity->setNombre($request->request->get('cargName'));
+					$em->persist($entity);
+					$em->flush();
+					
+					return new Response('!');
+				}
+				catch(\Exception $e)
+				{
+					return new Response('#');
+				}
+			}
+			else
+				return $this->redirect($this->generateUrl('as_secu'));
+		}
+		else
+			return $this->render("ScontrolBundle::alertas.html.twig");
 	}
 }
