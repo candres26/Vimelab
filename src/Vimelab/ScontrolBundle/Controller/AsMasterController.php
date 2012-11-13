ba375bcfdc58571a713ac795e7abcc9752cceea8
Javier Herran <ejherran.c@gamil.com>
2012-11-13 15:02:20 -0500

    Temporal AsMaster_02

--- src/Vimelab/ScontrolBundle/Controller/AsMasterController.php
+++ src/Vimelab/ScontrolBundle/Controller/AsMasterController.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Vimelab\ScontrolBundle\Tool\Tool;
 
+use Vimelab\ScontrolBundle\Entity\Gbpers;
+
 class AsMasterController extends Controller
 {
 	public function indexAction()
 	{
-		if(Tool::isGrant($this))	
-			return $this->render('ScontrolBundle:AsMaster:index.html.twig');
+		if(Tool::isGrant($this))
+		{
+			$secu = $this->get('security.context');
+			$ussys = $secu->getToken()->getUser();
+			
+			$em = $this->getDoctrine()->getEntityManager();
+			$entity = $em->getRepository('ScontrolBundle:Gbpers')->getForUser($ussys);
+				
+			return $this->render('ScontrolBundle:AsMaster:index.html.twig', array('GbPers' => $entity));
+		}
 		else
 			return $this->render('ScontrolBundle::alertas.html.twig');
 	}
